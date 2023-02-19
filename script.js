03e59cf1e2adbc096a61b379680f8591d2b506ae
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-02-19 20:10:08 +0530

    update experience and resume

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hi from Roopesh :)")
+
+let biography = {
+  fullName: "Roopesh Saravanan",
+  qualification: "B.Tech in Electical and Electronics Engineering",
+  currentRole: "Software Engineer Intern",
+  organization: "Pando",
+  college: "B.S. Abdur Rahman Crescent Institute of Science and Technology",
+  lastUpdated: "Feb 19, 2023",
+}
+
+console.log(biography)
